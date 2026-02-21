a20b995776330e4661c45d330b7053321f849374
solod3v <yunusemreyldz2007@gmail.com>
2026-02-21 23:23:14 +0300

    Simplify late move pruning logic in negamax by removing unnecessary en passant check
    bench: 1416370

--- search.cpp
+++ search.cpp
@@ -717,9 +717,7 @@ int negamax(Board& board, int depth, int alpha, int beta, int ply, std::vector<u
             depth <= params.lmp_max_depth &&
             movesSearched >= lmpCount &&
             !inCheck && get_promotion_type(move) == -1 && piece_at_sq(board, move_to(move)) == EMPTY) {
-            if (!(move_flags(move) == FLAG_EN_PASSANT)) {
-                continue; // skip this move (late move pruning)
-            }
+            continue; // skip this move (late move pruning)
         }
 
         // SEE PVS Pruning
